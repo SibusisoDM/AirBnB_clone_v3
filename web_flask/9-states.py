fabb9aead216d5b2ca5375dc43e6d5abbce536e4
SibusisoDM <sdmangwane@gmail.com>
2023-05-17 02:11:38 -0700

    all

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,46 +1,41 @@
 #!/usr/bin/python3
-"""web app"""
+""" start flask web app """
+from ..models import storage
+from ..models.state import State
 from flask import Flask, render_template
-from models import storage
-
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route('/states/<id>')
-def state_and_id(id=None):
-    """list cities and states"""
-
-    states = [st for st in storage.all('State').values()]
-    match_st = None
-    if id:
-        for st in states:
-            if st.id == id:
-                match_st = st
-    return render_template('9-states.html', state=match_st)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def all_states_with_cities_html():
-    """ all states and cities"""
-
-    states = [st for st in storage.all('State').values()]
-    return(render_template('8-cities_by_states.html', states=states))
-
-
-@app.route('/states')
-def states():
-    """access File/DB Storage for all State objects and render to HTML"""
-    states = [st for st in storage.all('State').values()]
-    return render_template('7-states_list.html', states=states)
-
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the database again at the end of the request."""
+def close_db(error):
+    """ should remove the current SQLAlchemy session """
     storage.close()
 
 
-if __name__ == '__main__':
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_state(id=""):
+    """ renders page with a list of cities by states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    found = 0
+    state = ""
+
+    for i in states:
+        if id == i.id:
+            state = i
+            found = 1
+            break
+    if found:
+        states = sorted(state.cities, key=lambda k: k.name)
+        state = state.name
+
+    if id and not found:
+        found = 2
+
+    return render_template('9-states.html', state=state, array=states, found=found)
+
+
+if __name__ == "__main__":
+    """ runs app """
     app.run(host='0.0.0.0', port=5000)
